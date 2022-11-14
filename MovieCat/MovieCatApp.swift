a97b85b5c81f46bfda25c8f171bf80e4e7f5f342
kz <michal@babicz.dev>
2022-11-14 08:58:00 +0100

    Initial Commit

---
+++ MovieCat/MovieCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCatApp.swift
+//  MovieCat
+//
+//  Created by kz on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
