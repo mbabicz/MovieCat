b13af1ac3fdd516248d0d8a6ff2776ffa7830f7b
kz <michal@babicz.dev>
2022-11-17 10:42:20 +0100

    sign up/in alerts

--- ContentView.swift
+++ ContentView.swift
@@ -15,45 +15,46 @@ class AppViewModel: ObservableObject{
     let auth = Auth.auth()
     
     @Published var signedIn = false
+    @Published var showingAlert : Bool = false
+    @Published var alertMessage = ""
     
     var isSignedIn: Bool {
         return auth.currentUser != nil
     }
     
     func signIn(email: String, password: String){
-        auth.signIn(withEmail: email, password: password){ [weak self] result, error in
-            guard result != nil, error == nil else{
-                return
+        auth.signIn(withEmail: email, password: password){ (result, error) in
+            if error != nil{
+                self.alertMessage = error?.localizedDescription ?? "Something went wrong"
+                self.showingAlert = true
+            } else {
+                DispatchQueue.main.async{
+                    //Success
+                    self.signedIn = true
+                }
             }
-            DispatchQueue.main.async{
-                //Success
-                self?.signedIn = true
-                
-            }
-            
         }
     }
     
     func signUp(email: String, password: String, username: String){
-        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
-            guard result != nil, error == nil else{
-                return
-            }
-            
-            self?.signedIn = true
-            
-            let userID = Auth.auth().currentUser!.uid
-            print(userID)
-            let firestoreDatabase = Firebase.Firestore.firestore()// Firebase.Firestore.firestore()
-            var firestoreReference : DocumentReference? = nil
-            
-            firestoreDatabase.collection("Users").document(userID).setData([
-                "username" : username,
-                "email" : email,
-                "date of registration" : Date.now,
-                "profile picture" : "firebasestorage.googleapis.com/v0/b/moviecat-59602.appspot.com/o/profile-picture.png?alt=media&token=f6f892c4-0725-47a9-8139-187809e26084"
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                self.alertMessage = error?.localizedDescription ?? "Something went wrong"
+                self.showingAlert = true
                 
-            ])
+            } else{
+                
+                self.signedIn = true
+                let userID = self.auth.currentUser!.uid
+                print(userID)
+                let firestoreDatabase = FirebaseFirestore.Firestore.firestore()
+                
+                firestoreDatabase.collection("Users").document(userID).setData([
+                    "username" : username,
+                    "email" : email,
+                    "date of registration" : Date.now
+                ])
+            }
         }
     }
     
@@ -63,21 +64,6 @@ class AppViewModel: ObservableObject{
         self.signedIn = false
     }
 }
-
-    
-//    func signUp(email: String, password: String, username: String){
-//        auth.createUser(withEmail: email, password: password){ [weak self] result, error in
-//            guard result != nil, error == nil else{
-//                return
-//            }
-//            //TODO: database userinfo
-//            //success
-//            self?.signedIn = true
-//
-//        }
-//    }
-//}
-
 
 struct ContentView: View {
         
@@ -111,44 +97,67 @@ struct ContentView: View {
         .onAppear{
             viewModel.signedIn = viewModel.isSignedIn
         }
-        
+        .alert(isPresented: $viewModel.showingAlert){
+            Alert(
+                title: Text("Error"),
+                message: Text(viewModel.alertMessage),
+                dismissButton: .default(Text("OK"))
+            )
+        }
     }
 }
 
 struct SignInView: View {
-    
     @State var email = ""
     @State var password = ""
 
     @EnvironmentObject var viewModel: AppViewModel
+    @State var isSecured: Bool = true
     
     var body: some View {
         VStack {
             VStack{
-                //Image
+                //TODO: Image
                 VStack{
                     TextField("Email Adress", text: $email).padding()                        .disableAutocorrection(true)
                         .autocapitalization(.none)
                         .background(Color(.secondarySystemBackground))
-                    
-                    SecureField("Password", text: $password).padding()                        .disableAutocorrection(true)
-                        .autocapitalization(.none)
-                        .background(Color(.secondarySystemBackground))
+                    ZStack(alignment: .trailing){
+                        Group{
+                            if isSecured {
+                                SecureField("Password", text: $password).padding()                        .disableAutocorrection(true)
+                                    .autocapitalization(.none)
+                                    .background(Color(.secondarySystemBackground))
+                            } else {
+                                TextField("Password", text: $password).padding()                        .disableAutocorrection(true)
+                                    .autocapitalization(.none)
+                                    .background(Color(.secondarySystemBackground))
+                            }
+                        }
+                        Button {
+                            isSecured.toggle()
+                        } label: {
+                            Image(systemName: self.isSecured ? "eye.slash" : "eye").accentColor(.gray)
+                        }.padding()
+                        
+                    }
                     
                     Button {
-                        guard !email.isEmpty, !password.isEmpty else {
-                            return
+                        if (!email.isEmpty && !password.isEmpty){
+                            viewModel.signIn(email: email, password: password)
+                        } else{
+                            viewModel.alertMessage = "Fields cannot be empty"
+                            viewModel.showingAlert = true
                         }
-                        viewModel.signIn(email: email, password: password)
 
                     } label: {
                         Text("Sign In").frame(width: 200, height: 50).bold().foregroundColor(Color.white).background(Color.blue).cornerRadius(8).padding()
                     }
+
                     Text("Don't have an account yet?")
                         .padding([.top, .leading, .trailing])
                     NavigationLink("Create Account", destination: SignUpView()).padding([.leading, .bottom, .trailing])
 
-                    
                 }
                 .padding()
                 Spacer()
@@ -163,14 +172,19 @@ struct SignUpView: View {
     
     @State var email = ""
     @State var password = ""
+    @State var passwordConfirmation = ""
     @State var username = ""
     
     @EnvironmentObject var viewModel: AppViewModel
     
+    @State var isSecured: Bool = true
+    @State var isSecuredConfirmation: Bool = true
+
+
     var body: some View {
         VStack {
             VStack{
-                //Image
+                //TODO: Image
                 VStack{
                     TextField("Username", text: $username).padding()
                         .disableAutocorrection(true)
@@ -181,22 +195,65 @@ struct SignUpView: View {
                         .autocapitalization(.none)
                         .background(Color(.secondarySystemBackground))
                     
-                    SecureField("Password", text: $password).padding()                        .disableAutocorrection(true)
-                        .autocapitalization(.none)
-                        .background(Color(.secondarySystemBackground))
+                    ZStack(alignment: .trailing){
+                        Group{
+                            if isSecured {
+                                SecureField("Password", text: $password).padding()                        .disableAutocorrection(true)
+                                    .autocapitalization(.none)
+                                    .background(Color(.secondarySystemBackground))
+                            } else {
+                                TextField("Password", text: $password).padding()                        .disableAutocorrection(true)
+                                    .autocapitalization(.none)
+                                    .background(Color(.secondarySystemBackground))
+                            }
+                        }
+                        Button {
+                            isSecured.toggle()
+                        } label: {
+                            Image(systemName: self.isSecured ? "eye.slash" : "eye").accentColor(.gray)
+                        }.padding()
+                        
+                    }
                     
+                    ZStack(alignment: .trailing){
+                        Group{
+                            if isSecuredConfirmation {
+                                SecureField("Password", text: $passwordConfirmation).padding()                        .disableAutocorrection(true)
+                                    .autocapitalization(.none)
+                                    .background(Color(.secondarySystemBackground))
+                            } else {
+                                TextField("Password", text: $passwordConfirmation).padding()                        .disableAutocorrection(true)
+                                    .autocapitalization(.none)
+                                    .background(Color(.secondarySystemBackground))
+                            }
+                        }
+                        Button {
+                            isSecuredConfirmation.toggle()
+                        } label: {
+                            Image(systemName: self.isSecuredConfirmation ? "eye.slash" : "eye").accentColor(.gray)
+                        }.padding()
+                        
+                    }
                     
                     Button {
-                        guard !email.isEmpty, !password.isEmpty, !username.isEmpty else {
-                            return
+                        if (!username.isEmpty && !email.isEmpty && !password.isEmpty && !passwordConfirmation.isEmpty ){
+                            if password == passwordConfirmation {
+                                viewModel.signUp(email: email, password: password, username: username)
+                            }
+                            else{
+                                viewModel.alertMessage = "Your password and confirmation password do not match"
+                                viewModel.showingAlert = true
+                            }
+                            
+                        } else {
+                            viewModel.alertMessage = "Fields cannot be empty"
+                            viewModel.showingAlert = true
                         }
-                        viewModel.signUp(email: email, password: password, username: username)
                         
                     } label: {
                         Text("Create Account").frame(width: 200, height: 50).bold().foregroundColor(Color.white).background(Color.blue).cornerRadius(8).padding()
                     }
 
-                    
                 }
                 .padding()
                 Spacer()
